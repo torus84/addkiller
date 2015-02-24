091697b57c77f98011fc014069ae6f6f1a6b55fc
Taras Shabatyn <taras.shabatin@gmail.com>
2015-02-24 13:29:27 +0200

    Adding BASE_DIR to activate.sh

--- activate.sh
+++ activate.sh
@@ -1,2 +1,17 @@
-#!\/bin\/bash
-
+#!/bin/bash
+realpath ()
+{
+    f=$@;
+    if [ -d "$f" ]; then
+        base="";
+        dir="$f";
+    else
+        base="/$(basename "$f")";
+        dir=$(dirname "$f");
+    fi;
+    dir=$(cd "$dir" && /bin/pwd);
+    echo "$dir$base"
+}
+BASE_DIR="$(dirname "${BASH_SOURCE[0]}")"
+BASE_DIR="$(realpath "$BASE_DIR")"
+export BASE_DIR
