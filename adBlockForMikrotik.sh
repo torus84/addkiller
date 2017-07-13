70a37fd0a0a27d48205b963daaaf5d416f878808
torus84 <taras.shabatin@gmail.com>
2017-07-13 15:20:00 +0300

    Update adBlockForMikrotik.sh

--- adBlockForMikrotik.sh
+++ adBlockForMikrotik.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 MIKROTIK_IP='192.168.88.1'
+MIKROTIK_ADMIN_NAME='admin'
+SUBSTITUDE_DOMAINE_IP='127.0.0.13'
 
 echo "Downloading hosts..."
 curl -s https://adaway.org/hosts.txt | grep "^127" | awk '{print $2}' > /tmp/ad-domains
@@ -11,19 +13,19 @@ dos2unix /tmp/ad-domains
 
 sort -u /tmp/ad-domains > /tmp/adblock-mikrotik.rsc
 
-sed -i 's/^/\/ip dns static add address="127.0.0.1" name="/g' /tmp/adblock-mikrotik.rsc
-sed -i 's/$/"/g' /tmp/adblock-mikrotik.rsc
+sed -i'' -e "s/^/\/ip dns static add address=\"$SUBSTITUDE_DOMAINE_IP\" name=\"/g" /tmp/adblock-mikrotik.rsc
+sed -i'' -e 's/$/"/g' /tmp/adblock-mikrotik.rsc
 
 echo "Got $(wc -l /tmp/adblock-mikrotik.rsc) lines"
 
 echo "Removing old entryes..."
-ssh $MIKROTIK_IP "/ip dns static remove [ find where address=\"127.0.0.1\" ]"
+ssh $MIKROTIK_ADMIN_NAME@$MIKROTIK_IP "/ip dns static remove [ find where address=\"127.0.0.1\" ]"
 
 echo "Uploading adblock-mikrotik.rsc..."
-scp /tmp/adblock-mikrotik.rsc $MIKROTIK_IP:/
+scp /tmp/adblock-mikrotik.rsc $MIKROTIK_ADMIN_NAME@$MIKROTIK_IP:/
 
 echo "Aplying new hosts..."
-ssh $MIKROTIK_IP "/import adblock-mikrotik.rsc"
+ssh $MIKROTIK_ADMIN_NAME@$MIKROTIK_IP "/import adblock-mikrotik.rsc"
 
 rm -f /tmp/adblock-mikrotik.rsc
 rm -f /tmp/ad-domains
